d950ade559f714fe54306bafd8e6b2f177ebbe7c
Hatch4 <156847166+Hatch4@users.noreply.github.com>
2026-09-02 23:31:31 -0400

    Refactor Requirements class and add getPercent method

--- js/requirements.js
+++ js/requirements.js
@@ -69,6 +69,10 @@ class Requirements {
 
         return Math.floor((totalMet / totalRequired) * 100);
     }
+    getPercent() {
+    const counts = this.countPieces();
+    return this.calculateProgress(counts);
+}
 
     // ---------------------------------------------------------
     // Update progress + check for completion
