7ce2df98c8873ed82d158ec236a16a99faba7777
Hatch4 <156847166+Hatch4@users.noreply.github.com>
2026-09-03 00:40:20 -0400

    Refactor Input class for drag and drop functionality

--- js/input.js
+++ js/input.js
@@ -6,14 +6,11 @@ class Input {
         this.camera = camera;
 
         this.draggingItem = null;
-        this.dragX = 0;
-        this.dragY = 0;
 
         canvas.addEventListener("pointerdown", (e) => this.onPointerDown(e));
     }
 
     onPointerDown(e) {
-        // Canvas click (not tray)
         const tile = this.camera.screenToIso(e.clientX, e.clientY);
         if (!tile) return;
 
@@ -22,24 +19,14 @@ class Input {
 
     handleDragMove(x, y, item) {
         this.draggingItem = item;
-        this.dragX = x;
-        this.dragY = y;
     }
 
     handleDragEnd(x, y, item) {
-        this.draggingItem = item;
-
-        this.attemptPlacementAtScreen(x, y, item);
-
-        this.draggingItem = null;
-    }
-
-    attemptPlacementAtScreen(screenX, screenY, item) {
-        const tile = this.camera.screenToIso(screenX, screenY);
+        const tile = this.camera.screenToIso(x, y);
         if (!tile) return;
 
-        this.grid.selected = tile;
+        Placement.attemptPlacement(this.grid, tile.x, tile.y, item);
 
-        this.grid.placeItem(tile.x, tile.y, item);
+        this.draggingItem = null;
     }
 }
